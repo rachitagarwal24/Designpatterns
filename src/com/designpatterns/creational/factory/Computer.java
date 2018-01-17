5fc1a1a6623f4032df2494e435b959dcc5b18975
Rachit Agarwal <rachit.agarwal@skilrock.com>
2018-01-17 20:50:26 +0530

    general commit

---
+++ src/com/designpatterns/creational/factory/Computer.java
@@ -0,0 +1,12 @@
+package com.designpatterns.creational.factory;
+
+/**
+ * Created by rachit on 17/1/18.
+ */
+public abstract class Computer {
+
+    public abstract String getRAM();
+    public abstract String getHDD();
+    public abstract String getCPU();
+
+}
