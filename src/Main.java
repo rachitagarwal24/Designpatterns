439b7035cb6ead18789e4d357116e6a03a21f03a
Rachit Agarwal <rachit.agarwal@skilrock.com>
2018-01-17 09:46:26 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
+        /*String s1="ONE";
+        String s2=new String("ONE");
+        String s3=s1;
+
+        System.out.println(System.identityHashCode(s2));
+        System.out.println(System.identityHashCode(s1));
+        System.out.println(System.identityHashCode(s3));
+        System.out.println(s1==s3);*/
         System.out.println("Hello World!");
     }
 }
