25c95de6f1fe0fb1d14006dbec52d9a2981d5835
Rachit Agarwal <rachit.agarwal@skilrock.com>
2018-02-05 20:27:06 +0530

    add .gitignore

---
+++ src/structural/adapter/Socket.java
@@ -0,0 +1,7 @@
+package structural.adapter;
+
+/**
+ * Created by rachit on 5/2/18.
+ */
+public class Socket {
+}
---
+++ src/structural/composite/Circle.java
@@ -0,0 +1,7 @@
+package structural.composite;
+
+/**
+ * Created by rachit on 5/2/18.
+ */
+public class Circle {
+}
