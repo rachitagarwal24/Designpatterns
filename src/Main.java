1ca96d4953b69de34ac9973739971e9d2ea7517a
unknown <kotarachit24@gmail.com>
2018-05-20 21:55:33 +0530

    Adding some code to main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,55 @@
+/*
+class A{
+    int doS(int a){
+        return 1;
+    }
+
+}
+class B extends A{
+    void  doS(int a){
+      return ;
+    }
+    public void s(){
+        doS(1);
+    }
+
+}
 public class Main {
+    int x=7;
+    int y=2;
+    enum Roman{I,R,V};
 
     public static void main(String[] args) {
-        /*String s1="ONE";
+String s1="ONE";
         String s2=new String("ONE");
         String s3=s1;
 
         System.out.println(System.identityHashCode(s2));
         System.out.println(System.identityHashCode(s1));
         System.out.println(System.identityHashCode(s3));
-        System.out.println(s1==s3);*/
+        System.out.println(s1==s3);
+
+    new Main();
+        System.out.println(i);
+
+
+        new B().s();
+
+
+
         System.out.println("Hello World!");
     }
+
+
+}
+*/
+
+
+public class Main{
+    public static void main(String args[]){
+        String s2 = "" + 1 + 1+ 1.0 + false;
+        System.out.println(s2);
+
+    }
+
 }
